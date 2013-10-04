8b8b52cc2fd3657452f88638a5e38efa025c249f
Florian Anderiasch <fa@art-core.org>
2013-10-04 21:02:09 +0200

    Fix dovecot for Debian Wheezy

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,9 +4,11 @@ class postfix::params {
   if $::osfamily == 'Debian' {
     $daemon_directory = '/usr/lib/postfix'
     $service_restart = '/usr/sbin/service postfix reload'
+    $dovecot_directory = '/usr/lib/dovecot'
   } else {
     $daemon_directory = '/usr/libexec/postfix'
     $service_restart = '/sbin/service postfix reload'
+    $dovecot_directory = '/usr/libexec/dovecot'
   }
 }
 
