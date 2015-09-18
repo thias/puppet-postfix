c5bd65e09082a6fe11b1a57c995677703905babf
markasammut <mark.sammut@gamesysnetwork.com>
2015-09-18 17:27:04 +0200

    correction to file name

--- manifests/server.pp
+++ manifests/server.pp
@@ -194,13 +194,12 @@ class postfix::server (
   }
   
   if $smtpd_recipient_access_list {
-	  file { "${config_directory}/${smtpd_recipient_access_list}" :
-	    content => template("postfix/access_list.erb"),
-	    notify  => Service['postfix'],
+    file { "${config_directory}/access_recipients" :
+      content => template("postfix/access_list.erb"),
+      notify  => Service['postfix'],
       require => Package[$package_name],
-	  }
-	}
-
+    }
+  }
 
   # Optional Spamassassin setup (using spampd)
   if $spamassassin {
